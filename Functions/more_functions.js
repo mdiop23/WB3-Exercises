ef78e287c73522448e3d0a4d9786d861ef66fcb6
mdiop23 <madina.diop20@gmail.com>
2023-10-29 21:14:41 -0400

    exercise 1-13

---
+++ Functions/more_functions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function displayMailingLabel(name, address, city, state, zip){
+    console.log("Name");
+    console.log("Address");
+    console.log("City");
+    console.log("State");
+    console.log("Zip");
+};
+
+function addNumbers(num1,num2){
+    let a = num1;
+    let b = num2;
+    let c = Math.abs(a-b);
+    console.log(c)
+
+}; 
+
+function displayReceipt(totalDue, amountPaid){
+};
+}
