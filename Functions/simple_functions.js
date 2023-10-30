9eb9a7a632714dd88501b9a39c8b1bc4e322d157
mdiop23 <madina.diop20@gmail.com>
2023-10-29 20:20:42 -0400

    simple function practice exercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+function favoriteThings() {
+    console.log("Madina");
+    console.log("SylviesLove");
+    console.log("MariahTheScientist"); 
+} 
+
+function whyImHere() {
+    let message = "I joined this LTC program to develop a lucrative skill set that will afford me an advanced understanding of software development and computer technology";
+    console.log(message);
+}
+
+function favoritePlace() {
+    let message = "My favorite place to visit so far has been Barcelona because of the rich culture, vibrant landsape, and phenomenal food.";
+    console.log(message);
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
